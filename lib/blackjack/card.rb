e01aa41278f4eb019f747e62ebb9e9d6eb7dbfd5
mdunbavan <mdunbavan@gmail.com>
2013-01-12 14:45:38 +0000

    Initial commit

---
+++ lib/blackjack/card.rb
@@ -0,0 +1,13 @@
+module Blackjack
+	class Card
+		#create a constant that will run throughout the application
+		SUITS = [:spades, :hearts, :diamonds, :clubs]
+		
+		attr_reader :rank, :suit
+		
+		def self.make_deck
+			deck = []
+			
+		end
+	end
+end
